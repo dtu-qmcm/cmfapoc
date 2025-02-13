6b6bd3b45255ca8a92c01c13c98e5e85208f3c71
Teddy Groves <groves.teddy@gmail.com>
2025-02-13 16:48:28 +0100

    Function for normalising

--- src/cmfapoc/data_preparation.py
+++ src/cmfapoc/data_preparation.py
@@ -1,11 +1,40 @@
 """Code for preparing data."""
 
+from collections.abc import Sequence
 from pathlib import Path
+
+from polars._typing import IntoExpr
 import polars as pl
 
 ROOT = Path(__file__).parent.parent.parent
 RAW_FILE = ROOT / "data" / "raw" / "PivotTable_freqs 1.csv"
 OUT_FILE = ROOT / "data" / "prepared" / "measurements.csv"
+
+
+def normalise(expr: pl.Expr, over: IntoExpr | Sequence[IntoExpr]):
+    """Normalise expression by sum over given columns.
+
+    For example:
+
+    >>> df = pl.DataFrame(
+        {
+            "a": [1, 2, 3],
+            "b": ["x", "x", "y"],
+        }
+    )
+    >>> df.with_columns(c=normalise(pl.col("a"), over=["b"]))
+        ┌─────┬─────┬──────────┐
+        │ a   ┆ b   ┆ c        │
+        │ --- ┆ --- ┆ ---      │
+        │ i64 ┆ str ┆ f64      │
+        ╞═════╪═════╪══════════╡
+        │ 1   ┆ x   ┆ 0.333333 │
+        │ 2   ┆ x   ┆ 0.666667 │
+        │ 3   ┆ y   ┆ 1.0      │
+        └─────┴─────┴──────────┘
+
+    """
+    return expr / expr.sum().over(over)
 
 
 def prepare_data(raw: pl.DataFrame) -> pl.DataFrame:
@@ -31,9 +60,9 @@ def prepare_data(raw: pl.DataFrame) -> pl.DataFrame:
             replicate_id=pl.col("measurement_id")
             .str.extract(r"ID_\d+_rep_(\d+)")
             .cast(int),
-            fraction=pl.col("raw_fraction")
-            / (
-                pl.col("raw_fraction").sum().over(["measurement_id", "species"])
+            fraction=normalise(
+                pl.col("raw_fraction"),
+                over=["measurement_id", "species"],
             ),
         )
         .with_columns(is_c12=pl.col("sample_id") == 0)
